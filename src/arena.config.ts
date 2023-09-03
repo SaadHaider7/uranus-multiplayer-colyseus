64c727d7a3dd9bb298d8f0a789a0019150537951
Leonidas <leo@solargames.io>
2023-09-03 17:03:18 +0300

    Updated the clustering distance for world rooms

--- src/arena.config.ts
+++ src/arena.config.ts
@@ -30,19 +30,19 @@ export default Arena({
         gameServer.define('relay_world_tiny', RelayRoom, {
             maxClients: 10,
             allowReconnectionTime: 3,
-            clusterDistance: 150.0
+            clusterDistance: 125.0
         }).filterBy(['areaId'])
 
         gameServer.define('relay_world_small', RelayRoom, {
             maxClients: 20,
             allowReconnectionTime: 3,
-            clusterDistance: 150.0
+            clusterDistance: 125.0
         }).filterBy(['areaId'])        
 
          gameServer.define('relay_world_room', RelayRoom, {
             maxClients: 50,
             allowReconnectionTime: 3,
-            clusterDistance: 150.0
+            clusterDistance: 125.0
         }).filterBy(['areaId'])        
     },
 
